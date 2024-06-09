1ca1d2afb98c0c171f7bb73abdb727c153c2b13a
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-09 12:43:12 -0300

    Criação dos Repositórios

---
+++ backend/src/main/java/com/financas/model/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.financas.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.financas.model.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    
+}
