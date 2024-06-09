e6bedefff73e455849dc26461e885506d83f691c
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-09 12:19:48 -0300

    Mapeamento das Entidades

---
+++ backend/src/main/java/com/financas/model/entity/Lancamento.java
@@ -0,0 +1,38 @@
+package com.financas.model.entity;
+
+import java.time.LocalDate;
+import com.financas.model.entity.enums.StatusLancamento;
+import com.financas.model.entity.enums.TipoLancamento;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "lancamento")
+@Data
+public class Lancamento {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private Integer mes, ano;
+    private Double valor;
+    private LocalDate dataCadastro;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Enumerated(value = EnumType.STRING)
+    private TipoLancamento tipo;
+
+    @Enumerated(value = EnumType.STRING)
+    private StatusLancamento status;
+}
---
+++ backend/src/main/java/com/financas/model/entity/Usuario.java
@@ -0,0 +1,18 @@
+package com.financas.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="usuario")
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String nome, email, senha;
+}
