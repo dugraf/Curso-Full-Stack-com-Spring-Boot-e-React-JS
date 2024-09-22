e45698c4e47609618547ce7ba695d072cc7a00b2
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-09-22 14:47:31 -0300

    Implementando o Datatable de lancamentos
    Este componente funcional exibe uma tabela com informações de lançamentos financeiros, como descrição, valor, tipo, mês e status. As linhas são geradas dinamicamente com base nos dados recebidos via props. A coluna "Ações" foi deixada em branco, e pode ser usada para botões ou links que permitam editar ou excluir os lançamentos.

--- frontend/src/views/lancamentos/consultaLancamentos.js
+++ frontend/src/views/lancamentos/consultaLancamentos.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import Card from "../components/card";
-import FormGroup from "../components/form-group";
-import SelectMenu from "../components/selectMenu";
+import Card from "../../components/card";
+import FormGroup from "../../components/form-group";
+import SelectMenu from "../../components/selectMenu";
+import LancamentosTable from "./lancamentosTable";
 
 class ConsultaLancamentos extends React.Component {
     render() {
@@ -27,6 +28,10 @@ class ConsultaLancamentos extends React.Component {
             {label: 'Despesa', value: 'DESPESA'},
             {label: 'Receita', value: 'RECEITA'},
         ]
+
+        const lancamentos = [
+            {id: 1, descricao: 'Salario', valor: 5000, mes: 1, tipo: 'Receita', status: 'Efetivado'}
+        ]
         return (
             <Card title = "Consulta Lançamentos">
                 <div className="row">
@@ -46,6 +51,19 @@ class ConsultaLancamentos extends React.Component {
                             <FormGroup htmlFor="inputTipo" label="Tipo Lançamento: ">
                                 <SelectMenu id="inputTipo" className="form-control" lista={tipos} />
                             </FormGroup>
+
+                            <button type="button" className="btn btn-success">Buscar</button>
+                            <button type="button" className="btn btn-danger">Cadastrar</button>
+                        </div>
+                    </div>
+                </div>
+
+                <br/>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="bs-component">
+                            <LancamentosTable lancamentos={lancamentos}/>
                         </div>
                     </div>
                 </div>
