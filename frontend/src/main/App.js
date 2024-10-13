f6473ff7118454bb49bc69fbc650af8d7548d5f8
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-10-13 13:40:46 -0300

    Adicionando e testando a biblioteca do Primereact no projeto
    npm install primereact@5.0.0
    npm install primeicons
    npm install classnames react-transition-group

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -8,11 +8,17 @@ import Navbar from '../components/narvbar'
 import 'toastr/build/toastr.css'
 import 'toastr/build/toastr.min.js'
 
+import {Button} from 'primereact/button'
+import 'primereact/resources/themes/nova/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
 class App extends React.Component{
   render() {
     return(
       <>
       <Navbar />
+      <Button label="Click" icon="pi pi-check" iconPos="right" />
       <div className="container">
         <Rotas />
       </div>
