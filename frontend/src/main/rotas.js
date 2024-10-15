070f60ed17e90d285cdd6743001928eac1a25d9d
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-10-14 23:06:23 -0300

    Criando o formulário de cadastro e atualização de Lançamentos

--- frontend/src/main/rotas.js
+++ frontend/src/main/rotas.js
@@ -4,6 +4,7 @@ import Login from '../views/login'
 import CadastroUsuario from '../views/cadastroUsuario'
 import Home from '../views/home'
 import ConsultaLancamentos from '../views/lancamentos/consultaLancamentos'
+import CadastroLancamentos from '../views/lancamentos/cadastroLancamentos'
 
 import {Route, Switch, HashRouter} from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function Rotas(){
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastroUsuario} />
                 <Route path="/consulta-lancamentos" component={ConsultaLancamentos} />
+                <Route path="/cadastro-lancamentos" component={CadastroLancamentos} />
             </Switch>
         </HashRouter>
     )
